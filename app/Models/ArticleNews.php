dd119e1f114a290da73ec5ec066306d2b3cb6eec
rstsfyn <restusofyanmaarif08@gmail.com>
2024-08-15 09:44:03 +0700

    Create : CRUD Article News

--- app/Models/ArticleNews.php
+++ app/Models/ArticleNews.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Support\Str;
 
 class ArticleNews extends Model
 {
@@ -20,7 +21,11 @@ class ArticleNews extends Model
         'author_id',
         'is_featured'
     ];
-
+    
+    public function setNameAttribute($value) {
+        $this->attributes['name'] = $value;
+        $this->attributes['slug'] = Str::slug($value);
+    }
     public function category(): BelongsTo {
         return $this->belongsTo(Category::class, 'category_id');
     }
